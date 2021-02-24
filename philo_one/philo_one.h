046e03f0e1655761ca00835f84687b9614936f3f
David ARBIB <darbib@student.42.fr>
2021-02-24 11:55:31 +0100

    philo_one init

---
+++ philo_one/philo_one.h
@@ -0,0 +1,13 @@
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+typedef struct	s_philo
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosophers_must_eat;
+}				t_philo;
+
+#endif
