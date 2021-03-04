2ceaccbf4098460e39310c7db1760a65b92c607c
David ARBIB <darbib@student.42.fr>
2021-03-04 16:09:48 +0100

    todo concurrent concatenation

--- philo_one/srcs/print.c
+++ philo_one/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: darbib <darbib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 13:25:27 by darbib            #+#    #+#             */
-/*   Updated: 2021/03/04 14:49:50 by darbib           ###   ########.fr       */
+/*   Updated: 2021/03/04 16:03:55 by darbib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <unistd.h>
 #include "philo_one.h"
+#include <stdio.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -41,25 +42,40 @@ int		ft_ltobuffer(unsigned long n, char *buf)
 {
 	size_t	i;
 	char	tmp;
+	size_t	len;
 
 	buf[20] = 0;
 	i = 19;
 	if (n == 0)
 		buf[i--] = '0';
-	while ((n >= 1 && n > 0) || (n <= -1 && n < 0))
+	while (n >= 1)
 	{
 		tmp = n % 10;
-		if (tmp < 0)
-			tmp = tmp * -1;
 		buf[i] = tmp + '0';
 		n /= 10;
 		i--;
 	}
-	ft_memmove(buf, buf + i, 19 - i);
-	return ((int)(19 - i));
+	len = 19 - i;
+	ft_memmove(buf, buf + i + 1, len);
+	if (i > -1)
+		memset(buf + len, 0, i);
+	return ((int)(len));
 }
 
-char	*concatenate_philo_message(t_philo *philo, char *message)
+/*
+int main()
+{
+	char buf[30];
+	bzero(buf, 30);
+	unsigned long a = 23454546457;
+	int len = ft_ltobuffer(a, buf);
+	printf("%s\n", buf);
+	printf("%d\n", len);
+	return (0);
+}
+*/
+
+void	concatenate_philo_message(t_philo *philo, char *message)
 {
 	int		timestamp_usec;
 	int		count;	
@@ -67,9 +83,8 @@ char	*concatenate_philo_message(t_philo *philo, char *message)
 	memset(message, 0, 50);
 	timestamp_usec = get_usec_from_epoch();
 	count = ft_ltobuffer(timestamp_usec, message);	
-	message[count] = ' ';
+	message[count++] = ' ';
 	count += ft_ltobuffer(philo->id, message + count);	
-	message[count] = ' ';
 	if (philo->state == thinking)
 		ft_memmove(message + count, " is thinking\n", 13);
 	if (philo->state == eating)
@@ -81,14 +96,14 @@ char	*concatenate_philo_message(t_philo *philo, char *message)
 	if (philo->state == sleeping)
 		ft_memmove(message + count, " is sleeping\n", 13);
 	if (philo->state == dead)
-		ft_memmove(message + count, " died\n", 6);	
-	return (message);
+		ft_memmove(message + count, " died\n", 6);
 }
 
+//void	print_state(t_philo *philo, t_param *param)
 void	print_state(t_philo *philo)
 {
 	char	message[50];
 
-	message = concatenate_philo_message(philo, message);
+	concatenate_philo_message(philo, message);
 	write(1, message, ft_strlen(message));
 }
